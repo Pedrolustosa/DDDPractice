f2c52ebdc34b95929fd9bfe97c323ab217a14425
Pedrolustosa <pedroeternalss@gmail.com>
2023-11-25 14:29:58 -0300

    feat: Implement UserService

---
+++ DDDPractice.Service/Services/UserService.cs
@@ -0,0 +1,42 @@
+using DDDPractice.Domain.Entities;
+using DDDPractice.Domain.Interfaces;
+using DDDPractice.Domain.Interfaces.Services.User;
+
+namespace DDDPractice.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<UserEntity> _repository;
+
+        public UserService(IRepository<UserEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            return await _repository.SelectAllAsync();
+        }
+
+        public async Task<UserEntity> Get(Guid id)
+        {
+            return await _repository.SelectAsync(id);
+        }
+
+        public async Task<UserEntity> Post(UserEntity userEntity)
+        {
+            return await _repository.InsertAsync(userEntity);
+        }
+
+        public async Task<UserEntity> Put(UserEntity userEntity)
+        {
+            return await _repository.UpdateAsync(userEntity);
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+    }
+}
