00182e04d548d110c8b255f244f757e332f56a31
Pedrolustosa <pedroeternalss@gmail.com>
2023-11-27 16:07:49 -0300

    feat: Create GetAll method

--- DDDPractice.Application/Controllers/UserController.cs
+++ DDDPractice.Application/Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using DDDPractice.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DDDPractice.Application.Controllers
@@ -6,6 +8,18 @@ namespace DDDPractice.Application.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService userService)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+            try
+            {
+                return Ok(await userService.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
     }
 }
