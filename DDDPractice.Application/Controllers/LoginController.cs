688c9f1112a0d97d653de00751c795af461c4270
Pedrolustosa <pedroeternalss@gmail.com>
2023-11-29 20:19:41 -0300

    feat: Create LoginController

---
+++ DDDPractice.Application/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using DDDPractice.Domain.Entities;
+using DDDPractice.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDDPractice.Application.Controllers
+{
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<object> Login([FromBody] UserEntity userEntity, [FromServices] ILoginService loginService)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            if(userEntity is null)
+                return BadRequest(userEntity);
+            
+            try
+            {
+                var result = await loginService.FindByLogin(userEntity);
+                if(result is not null)
+                    return Ok(result);
+                else
+                    return NotFound();
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
