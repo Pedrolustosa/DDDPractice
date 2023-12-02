2eb869bde0bf13a905d9ca7e851e65ff1251dbc6
Pedrolustosa <pedroeternalss@gmail.com>
2023-12-02 13:24:18 -0300

    feat: Implement SigningConfigurations and TokenConfigurations class

---
+++ DDDPractice.Domain/Security/TokenConfigurations.cs
@@ -0,0 +1,10 @@
+#nullable disable
+namespace DDDPractice.Domain.Security
+{
+    public class TokenConfigurations
+    {
+        public string Audience { get; set; }    
+        public string Issuer { get; set; }
+        public int Seconds { get; set; }
+    }
+}
