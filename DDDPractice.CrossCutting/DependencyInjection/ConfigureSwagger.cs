7e77f1a4570bdb6e1a0c6546659d6c1210253f2e
Pedrolustosa <pedroeternalss@gmail.com>
2023-12-02 16:15:49 -0300

    feat: Add authorize button to swagger

--- DDDPractice.CrossCutting/DependencyInjection/ConfigureSwagger.cs
+++ DDDPractice.CrossCutting/DependencyInjection/ConfigureSwagger.cs
@@ -21,6 +21,34 @@ namespace DDDPractice.CrossCutting.DependencyInjection
                         Url = new Uri("https://www.linkedin.com/in/pedrolustosaengineer/")
                     }
                 });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = @"JWT Authorization header using Bearer.
+                                    Enter 'Bearer' [space] then put in your token.
+                                    Example: 'Bearer 12345abcdef'",
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
             return services;
         }
