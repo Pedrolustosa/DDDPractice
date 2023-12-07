3ec1623a0dc915f7590486187ba5613c76c77587
Pedrolustosa <pedroeternalss@gmail.com>
2023-12-07 20:37:30 -0300

    feat: Finalize User CRUD Testing

--- DDDPractice.Data.Test/UserCRUDComplete.cs
+++ DDDPractice.Data.Test/UserCRUDComplete.cs
@@ -47,6 +47,32 @@ namespace DDDPractice.Data.Test
             Assert.Equal(_entity.Name, _createdRecord.Name);
             Assert.Equal(_entity.Email, _createdRecord.Email);
             Assert.False(_createdRecord.Id == Guid.Empty);
+
+            _entity.Name = Faker.Name.First();
+            var _updateRecord = await _repository.UpdateAsync(_entity);
+            Assert.NotNull(_updateRecord);
+            Assert.Equal(_entity.Name, _updateRecord.Name);
+            Assert.Equal(_entity.Email, _updateRecord.Email);
+
+            var _existRecord = await _repository.ExistAsync(_createdRecord.Id);
+            Assert.True(_existRecord);
+
+            var _selectedRecord = await _repository.SelectAsync(_createdRecord.Id);
+            Assert.NotNull(_selectedRecord);
+            Assert.Equal(_entity.Name, _selectedRecord.Name);
+            Assert.Equal(_entity.Email, _selectedRecord.Email);
+
+            var _selectedAllRecord = await _repository.SelectAllAsync();
+            Assert.NotNull(_selectedAllRecord);
+            Assert.True(_selectedAllRecord.Count() > 1);
+
+            var _removeRecord = await _repository.DeleteAsync(_selectedRecord.Id);
+            Assert.True(_removeRecord);
+
+            var _userStandard = await _repository.FindByLogin("test@test.com");
+            Assert.NotNull(_userStandard);
+            Assert.Equal("test", _selectedRecord.Name);
+            Assert.Equal("test@test.com", _selectedRecord.Email);
         }
     }
 }
